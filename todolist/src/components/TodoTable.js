b581036d56156330d14be45211be58d44a5ca872
Sauli Haapaniemi <saukkohaapaniemi@gmail.com>
2022-10-12 17:41:59 +0300

    ag-grid tehtäväpalautus + aiempia commitoimattomia muutoksia

--- todolist/src/components/TodoTable.js
+++ todolist/src/components/TodoTable.js
@@ -1,25 +1,29 @@
 import React from "react";
 
+// code formatted with Prettier
+
 function TodoTable(props) {
-    return(
-        <div>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Description</th>
-                        <th>Date</th>
-                    </tr>
-                    {
-                        props.todos.map((todo, index) =>
-                            <tr key={index}>
-                                <td>{todo.description}</td>
-                                <td>{todo.date}</td>
-                                <td><button onClick={() => props.deleteTodo(index)}>Delete</button></td>
-                            </tr>)
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Description</th>
+            <th>Date</th>
+          </tr>
+          {props.todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.description}</td>
+              <td>{todo.date}</td>
+              <td>
+                <button onClick={() => props.deleteTodo(index)}>Delete</button>
+              </td>
+              <td>{todo.priority}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default TodoTable;
--- todolist/src/components/Todolist.js
+++ todolist/src/components/Todolist.js
@@ -1,48 +1,105 @@
-import React, {useState} from "react";
-import TodoTable from'./TodoTable';
+import React, { useState, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
 
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+
+// code formatted with Prettier
 
 function Todolist() {
-    const [todos, setTodos] = useState([]);
-    const [todo, setTodo] = useState({
-        description: "",
-        date: ""
-    });
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState({
+    description: "",
+    date: "",
+    priority: "",
+  });
+  const gridRef = useRef();
 
-    const addTodo = (e) => {
-        e.preventDefault();
-        setTodos([todo, ...todos]);
-    }
-    const deleteTodo = (row) => {
-        setTodos(todos.filter((item, index) => row !== index))
-        console.log("Button pressed in " + row)
-    }
+  const [columnDefs] = useState([
+    {
+      field: "description",
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+      cellStyle: { color: "white", background: "black" },
+    },
+    { field: "date", sortable: true, filter: true, floatingFilter: true },
+    {
+      field: "priority",
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+      cellStyle: (params) =>
+        params.value.toLowerCase() === "high"
+          ? { color: "red", background: "black" }
+          : { color: "white", background: "black" },
+    },
+  ]);
 
-    const inputChanged = (e) => {
-        setTodo({...todo, [e.target.name]: e.target.value});
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([todo, ...todos]);
+  };
+  const deleteTodo = () => {
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(
+        todos.filter(
+          (todo, index) =>
+            index !== gridRef.current.getSelectedNodes()[0].childIndex
+        )
+      );
+    } else {
+      alert("No rows selected");
     }
+  };
 
-    return(
-        <div><header>List of things to do</header>
-            <form onSubmit={addTodo}>
-            Description:
-            <input
-            type="text"
-            name="description"
-            value={todo.description}
-            onChange={inputChanged}
-            />
-            Date:
-            <input
-            type="date"
-            name="date"
-            value={todo.date}
-            onChange={inputChanged}
-            />
-            <input type="submit" value="Add"></input>
-            </form>
-            <TodoTable todos={todos} deleteTodo={deleteTodo}/>
-        </div>
-    );
+  const inputChanged = (e) => {
+    setTodo({ ...todo, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <header>List of things to do</header>
+      <form onSubmit={addTodo}>
+        Description:&nbsp;
+        <input
+          type="text"
+          name="description"
+          value={todo.description}
+          onChange={inputChanged}
+        />
+        &nbsp; Date:&nbsp;
+        <input
+          type="date"
+          name="date"
+          value={todo.date}
+          onChange={inputChanged}
+        />
+        &nbsp; Priority:&nbsp;
+        <input
+          type="text"
+          name="priority"
+          value={todo.priority}
+          onChange={inputChanged}
+        />
+        &nbsp;
+        <input type="submit" value="Add"></input>
+        <input type="button" value="Delete" onClick={deleteTodo}></input>
+      </form>
+      <div
+        className="ag-theme-material"
+        style={{ margin: "auto", width: "50%", height: 600 }}
+      >
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={(params) => (gridRef.current = params.api)}
+          rowSelection="single"
+          rowData={todos}
+          columnDefs={columnDefs}
+          animateRows={true}
+        />
+      </div>
+    </div>
+  );
 }
 export default Todolist;
