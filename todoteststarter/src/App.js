6aa27af598a620eda5343d3a1f076e4b8dfee2de
Sauli Haapaniemi <saukkohaapaniemi@gmail.com>
2022-11-01 13:18:48 +0200

    testaus-tehtäväpalautus

---
+++ todoteststarter/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./App.css";
+import TodoTable from "./TodoTable";
+
+function App() {
+  const [todo, setTodo] = useState({ desc: "", date: "" });
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo({ desc: "", date: "" });
+  };
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Date"
+        name="date"
+        value={todo.date}
+        onChange={inputChanged}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        name="desc"
+        value={todo.desc}
+        onChange={inputChanged}
+      />
+      <button onClick={addTodo}>Add</button>
+      <TodoTable todos={todos} />
+    </div>
+  );
+}
+
+export default App;
---
+++ todoteststarter/src/App.test.js
@@ -0,0 +1,36 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import TodoTable from "./TodoTable";
+import App from "./App.js";
+
+test("renders todotable", () => {
+  const row = [{ desc: "Go to coffee", date: "24.01.2021" }];
+  render(<TodoTable todos={row} />);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
+});
+
+test("add todo", () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText("Description");
+  fireEvent.change(desc, { target: { value: "Go to coffee" } });
+  const date = screen.getByPlaceholderText("Date");
+  fireEvent.change(date, { target: { value: "29.01.2021" } });
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
+});
+
+// extra testi
+test("todo not in document", () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText("Description");
+  fireEvent.change(desc, { target: { value: "random" } });
+  const date = screen.getByPlaceholderText("Date");
+  fireEvent.change(date, { target: { value: "29.01.2021" } });
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
+  const tablecell = screen.queryByText(/random test/i);
+  expect(tablecell).not.toBeInTheDocument();
+});
